f69695631db6012b2d31672569ff038f3fb756ea
Fabrice Laporte <kraymer@gmail.com>
2017-07-22 14:54:32 +0200

    Launch cmd on each track instead of album

--- spotassium.py
+++ spotassium.py
@@ -7,7 +7,7 @@ import time
 from itertools import groupby
 from subprocess import call
 
-from .player import is_playing
+from player import is_playing
 
 config = confuse.Configuration('spotassium', __name__)
 config.add({
@@ -19,10 +19,26 @@ WAIT_PERIOD = 300  # 5 min
 
 
 def run_command_on_spotify_id(spotify_id):
-    """Run external command on given id"""
+    """Run external command on given track id"""
     placeholder = '<SPOTIFY_ID>'
+    print(config['command'].get())
     cmd = config['command'].get().replace(placeholder, spotify_id)
     call(cmd.split(' '))
+
+
+def do_playlist(playlist_id):
+    """Iterate on every track of playlist to run command on it.
+       Remove track from playlist if command successful.
+    """
+    results = sp.user_playlist(username, playlist_id,
+                               fields="tracks,next")
+    for album, tracks in groupby(results['tracks']['items'],
+                                 key=lambda x: x['track']['album']['uri']):
+        for track in list(tracks):
+            run_command_on_spotify_id(track['track']['uri'])
+            # sp.user_playlist_remove_all_occurrences_of_tracks(username,
+            #     playlist_id, [track['track']['id'],])
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -32,7 +48,7 @@ if __name__ == '__main__':
         print "usage: python user_playlists.py [username]"
         sys.exit()
 
-    token = util.prompt_for_user_token(username)
+    token = util.prompt_for_user_token(username, 'playlist-modify-public')
 
     if token:
         print('Access token: %s' % token)
@@ -43,11 +59,7 @@ if __name__ == '__main__':
             if not is_playing():
                 for playlist in playlists:
                     if playlist['name'] == config['playlist_name'].get():
-                        results = sp.user_playlist(username, playlist['id'],
-                                                   fields="tracks,next")
-                        for album, tracks in groupby(results['tracks']['items'],
-                                                     key=lambda x: x['track']['album']['uri']):
-                            run_command_on_spotify_id(album)
+                        do_playlist(playlist['id'])
             time.sleep(WAIT_PERIOD)
     else:
         print "Can't get token for", username
